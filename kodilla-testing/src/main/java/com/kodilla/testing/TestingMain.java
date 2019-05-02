229f1518212a5dfe4d60c546891ceaa454558258
HoneyRum <celinski.konrad95@gmail.com>
2019-05-02 18:03:59 +0200

    User and calculator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,32 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania programowania.");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //Kolejne testy
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(4, 2);
+        int subtractResult = calculator.subtract(4, 2);
+
+        if(addResult==6 && subtractResult==2) {
+            System.out.println("Calculator test OK");
+        } else {
+            System.out.println("Calculator error!");
+        }
     }
 }
