b9dd3c70ab9b7f7c52ad9ac69a12f34262eb7f64
HoneyRum <celinski.konrad95@gmail.com>
2019-07-13 10:03:53 +0200

    7.5, 7.6

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -3,12 +3,11 @@ package com.kodilla.stream.array;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
-    public static double getAverage(int[] numbers){
+    static double getAverage(int[] numbers){
         IntStream.range(0, numbers.length)
                 .forEach(System.out::println);
-        double average = IntStream.range(0, numbers.length)
+        return IntStream.range(0, numbers.length)
                 .average()
-                .getAsDouble();                                 //isPresent()?
-        return average;
+                .orElse(0.0);
     }
 }
